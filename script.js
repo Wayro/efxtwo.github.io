2960467b960f71df46210a1b669a8be58fa04948
Wayro <mickey@wayro.com>
2022-03-17 15:50:52 -0400

    shuffle fix; card count fix;
    fixed constant shuffle;
    fixed player and dealer count;

--- script.js
+++ script.js
@@ -11,22 +11,24 @@ const dealerValue = document.getElementById('dealerValue');
 let cardSuites = ['clubs','hearts','diamonds','spades'];
 let cardNums = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'];
 let deck = [];
+let initialGame = true;
 let cardCount = 0;
 let gameStatus = 0;
 let dealerHidden = 0;
 let dealerUp = 0;
 
+function createdeck(){
+    for(suit in cardSuites){
+        for(nums in cardNums){
+        let cardValue = nums > 9 ? 10 : parseInt(nums) + 1;
 
-for(suit in cardSuites){
-    for(nums in cardNums){
-       let cardValue = nums > 9 ? 10 : parseInt(nums) + 1;
-
-       let card = {
-           suites: cardSuites[suit],
-           number: cardNums[nums],
-           value: cardValue
-       };  
-       deck.push(card);
+        let card = {
+            suites: cardSuites[suit],
+            number: cardNums[nums],
+            value: cardValue
+        };  
+        deck.push(card);
+        }
     }
 }
 function shuffleDeck(array) {    //shuffling the deck, order of cards get re-arranged
@@ -43,47 +45,58 @@ function dealCards(){
     let playersHand =[];
     let dealersHand = [];
     let dealerCounter = 0;
+	let dealersUpValue = 0;
     gameStatus = 1;
     playersHold.innerHTML = '';
     dealersHold.innerHTML = '';
-
+    if(initialGame){
+		createdeck();
+		shuffleDeck(deck);
+		cardCount = 0;
+		initialGame = false;
+	}
     
     for(i=0; i<2; i++){
         playersHand.push(deck[cardCount]);
         playersHold.innerHTML += `<img src=PNG-cards/${deck[cardCount].number}_of_${deck[cardCount].suites}.png>`;
         // playerCounter = parseInt(playerCounter) + parseInt(`${deck[cardCount].value}`);
-        cardCounter();
+        cardCount++;
         dealersHand.push(deck[cardCount]);
         if (i == 0){
             dealersHold.innerHTML += `<img src=PNG-cards/pokemoncard.png>`;
             dealerHidden = cardCount;
         } else{
             dealersHold.innerHTML += `<img src=PNG-cards/${deck[cardCount].number}_of_${deck[cardCount].suites}.png>`;
-            dealerValue.innerHTML = `${deck[cardCount].value}`;
+            dealersUpValue = parseInt(dealersUpValue)+parseInt(`${deck[cardCount].value}`);
             dealerUp = cardCount;
         }
-        cardCounter();
+        cardCount++;
     }
     let playerCounter = aceCheck(playersHand);
     dealerCounter = aceCheck(dealersHand);
     if ((playerCounter == 21 && playersHand.length == 2) || (dealerCounter == 21 && dealersHand.length == 2)){
         dealerCounter = dealerCounter;
-        gameOver(dealerCounter);
-    }
-    playerValue.innerHTML = playerCounter;
+        gameOver(dealerCounter, playerCounter);
+    } else {
+		playerValue.innerHTML = playerCounter;
+		if(dealersUpValue == 1){
+			dealerValue.innerHTML = '1 or 11';
+		} else {
+			dealerValue.innerHTML = dealersUpValue;
+		}
+	}
+    cardCounter();
 }
 
 function cardCounter(){
-    cardCount++;
     if (cardCount > 29){
-        shuffleDeck(deck);
-        cardCount = 0;
+        initialGame = true;
     }
 }
 
 function startGame(){
-    shuffleDeck(deck);
     dealCards();
+    //console.log(JSON.stringify(deck))
 };
 
 function aceCheck(whosCard){
@@ -105,9 +118,11 @@ function aceCheck(whosCard){
        return checkValue; 
 }
 
-function gameOver(dealerCount){
+function gameOver(dealerCount, playerCount){
+    gameStatus = 0;
     dealersHold.innerHTML = `<img src=PNG-cards/${deck[dealerHidden].number}_of_${deck[dealerHidden].suites}.png><img src=PNG-cards/${deck[dealerUp].number}_of_${deck[dealerUp].suites}.png>`;
     dealerValue.innerHTML = dealerCount;
+    playerValue.innerHTML = playerCount;
 }
 
 startButton.addEventListener('click',startGame)
